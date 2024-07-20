141b72f404739421a5dcc5ab0ec078bdb6dd3f12
Dino <167688937+Single-eyed-Dino@users.noreply.github.com>
2024-07-20 23:56:14 +0530

    UPLOADING VARIABLES

---
+++ main (2).c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    // VARIABLE DECLARATION
+    int a;
+    char b;
+    
+    // ASSIGNING VALUES TO THE VARIABLES
+    a = 1;
+    b = 'f';
+    
+    // PRINTING THE VARIABLES
+    printf("%d, %c", a, b);
+}

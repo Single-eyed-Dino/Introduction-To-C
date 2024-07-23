191440a8c2c843d85b26c3fa5a874a3fb8212b9d
Dino <167688937+Single-eyed-Dino@users.noreply.github.com>
2024-07-23 23:10:33 +0530

    ADDING VARIABLES.

---
+++ main (2).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b;
+    
+    
+    printf("Enter the value of a: ");
+    
+    scanf("%d", &a);
+    
+    printf("Enter the value of b: ");
+    
+    scanf("%d", &b);
+    
+    printf("%d + %d = %d", a , b, a+b);
+}

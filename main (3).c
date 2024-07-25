9520f71a8c730eccd6079e04aa70b970fd06a540
Dino <167688937+Single-eyed-Dino@users.noreply.github.com>
2024-07-26 01:14:08 +0530

    STARTING ON NEXT LINE.c

---
+++ main (3).c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b;
+    
+    
+    printf("Enter the value of a: ");
+    
+    scanf("%d", &a);
+    
+    printf("Enter the value of b: ");
+    
+    scanf("%d", &b);
+    
+    printf("%d + %d = %d \n", a , b, a+b);
+    
+    printf("adding %d with %d = %d",a ,b, a+b);
+}
